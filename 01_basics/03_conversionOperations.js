4e1f1b92726a1d51c241f6fa6e065a0384ff7ce1
Tauheed Khan <144790752+tauheed5260@users.noreply.github.com>
2025-02-26 09:17:13 +0000

    Convertion Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,10 +1,10 @@
 let score = "Tauheed"
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33" => 33
 //"33abc" => NaN
@@ -19,5 +19,29 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//Operations//
+let value = 3
+let negValue = -value
+//console.log(negValue);
+let str1 = "hello"
+let str2 = " bhai"
+let str3 = str1 +  str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"3");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
