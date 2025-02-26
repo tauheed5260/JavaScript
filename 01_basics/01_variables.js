43de504d65b36d966b147e0b0ac3a137f038ac71
Tauheed Khan <144790752+tauheed5260@users.noreply.github.com>
2025-02-26 08:08:29 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 123456
+let accountEmail = "tauheed5260@gmail.com"
+var accountPassword = "54321"
+accountCity = "Allahabad"
+let accountState;
+
+// accountId = 2 // not allowed
+accountEmail = "hdfc@gmail.com"
+accountPassword = "76543"
+accountCity = "Ghazipur"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
