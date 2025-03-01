36a27006d803a207cb29943662f19abb92187655
Tauheed Khan <144790752+tauheed5260@users.noreply.github.com>
2025-03-01 06:37:34 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,30 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber = 196.1234
+// console.log(otherNumber.toPrecision(3));
+// const hundreds = 100000
+// console.log(hundreds.toLocaleStrig('en-In'));
+
+//Maths
+
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(4,5,6,7));
+// console.log(Math.max(8,7,6,5.2));
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1)))
