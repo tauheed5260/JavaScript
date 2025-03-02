bbb8e24b8c64f33931edc419e3ffab8b2bf272ba
Tauheed Khan <144790752+tauheed5260@users.noreply.github.com>
2025-03-02 06:57:50 +0000

    onjects part2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,38 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+tinderUser.Id = "5260"
+tinderUser.name = "Danny"
+tinderUser.IsLoggedIn = false
+
+
+// console.log(tinderUser);
+const regularUser = {
+    email: "random@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Tauheed",
+            lastname: "Khan", 
+        } 
+    }
+}
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.userfullname.firstname);
+const obj1 = {1: "a",2: "b"}
+const obj2 = {3: "c",4: "d"}
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({},obj1, obj2)
+const obj3 = {...obj1,...obj2}
+// console.log(obj3);
+const users = [
+    {
+        id: 1,
+        email: "tk5260@gmail.com"
+    },
+]
+users[0].email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('IsLoggedIn'));
