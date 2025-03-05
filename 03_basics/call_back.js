9dae858eddaee25a3699640fcdd8c765af9ee493
Tauheed Khan <144790752+tauheed5260@users.noreply.github.com>
2025-03-05 18:15:15 +0000

    call back

---
+++ 03_basics/call_back.js
@@ -0,0 +1,12 @@
+let val1 = 10
+let val2 = 2
+function addNum(num1, num2){
+    let total = num1 + num2 
+    return total
+}
+let result1 = addNum(val1, val2)
+let result2 = addNum(5, 8)
+console.log(result1);
+console.log(result2);
+
+//All the steps are done in snippet in the inspect section.
