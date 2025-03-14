ecabb97e2a06100a5f2fc42b8b958761fbc9a805
Tauheed Khan <144790752+tauheed5260@users.noreply.github.com>
2025-03-14 09:56:48 +0000

    do while

---
+++ 04_iterators/two.js
@@ -0,0 +1,17 @@
+let index = 0
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 1
+}
+let myArr = ["Tauheed", "Faiz", "Umar"]
+let arr = 0
+while (arr < myArr.length) {
+    // console.log(`value is ${myArr[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score ++
+} while (score <= 19);
